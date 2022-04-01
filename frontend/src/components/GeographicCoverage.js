a1a56174225e022796dc1689169395890931bdfe
jaeeunlim <esther.lim.719@gmail.com>
2022-04-01 15:40:14 -0400

    Add USA coverage map to GeographicCoverage component.

--- frontend/src/components/GeographicCoverage.js
+++ frontend/src/components/GeographicCoverage.js
@@ -1,3 +1,28 @@
+import USAMap from 'react-usa-map';
+import './../styles/GeographicCoverage.css';
+
+
 export default function GeographicCoverage(props) {
-    return <h1>Geographic Coverage</h1>;
+    function mapHandler(event) {
+        alert(event.target.dataset.name);
+    };
+
+    function statesCustomConfig() {
+        return {
+            "NJ": {
+                fill: "navy",
+                clickHandler: (event) => console.log('Custom handler for NJ', event.target.dataset)
+            },
+            "NY": {
+                fill: "#CC0000"
+            }
+        };
+    };
+
+    return (
+        <div>
+            <h1>Geographic Coverage</h1>
+            <USAMap customize={statesCustomConfig()} onClick={mapHandler} />
+        </div>
+    );
 }
