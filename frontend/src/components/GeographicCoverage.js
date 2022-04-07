52a54ee475495c05bff7217bcb7130ec1b748697
jaeeunlim <esther.lim.719@gmail.com>
2022-04-07 13:13:51 -0400

    Fetch and display world covid data from backend.

--- frontend/src/components/GeographicCoverage.js
+++ frontend/src/components/GeographicCoverage.js
@@ -1,7 +1,91 @@
 import { useEffect, useState } from 'react';
 import USAMap from 'react-usa-map';
+import WorldMap from 'react-svg-worldmap';
 import './../styles/GeographicCoverage.css';
 
+const { overwrite, getCode, getName } = require('country-list');
+overwrite([{
+    code: 'US',
+    name: 'United States'
+}, {
+    code: 'KR',
+    name: 'South Korea'
+}, {
+    code: 'TW',
+    name: 'Taiwan'
+}, {
+    code: 'GB',
+    name: 'United Kingdom'
+}, {
+    code: 'CD',
+    name: 'Democratic Republic of Congo'
+}, {
+    code: 'TZ',
+    name: 'Tanzania'
+}, {
+    code: 'IR',
+    name: 'Iran'
+}, {
+    code: 'VN',
+    name: 'Vietnam'
+}, {
+    code: 'LA',
+    name: 'Laos'
+}, {
+    code: 'RU',
+    name: 'Russia'
+}, {
+    code: 'BO',
+    name: 'Bolivia'
+}, {
+    code: 'VE',
+    name: 'Venezuela'
+}, {
+    code: 'RS',
+    name: 'Serbia'
+}, {
+    code: 'BN',
+    name: 'Brunei'
+}, {
+    code: "CV",
+    name: "Cape Verde"
+}, {
+    code: "CI",
+    name: "Cote d'Ivoire"
+}, {
+    code: "CW",
+    name: "Curacao"
+}, {
+    code: "BQ",
+    name: "Bonaire Sint Eustatius and Saba"
+}, {
+    code: "VG",
+    name: "British Virgin Islands"
+}, {
+    code: "FK",
+    name: "Falkland Islands"
+}, {
+    code: "FM",
+    name: "Micronesia (country)"
+}, {
+    code: "MD",
+    name: "Moldova"
+}, {
+    code: "PS",
+    name: "Palestine"
+}, {
+    code: "SH",
+    name: "Saint Helena"
+}, {
+    code: "SY",
+    name: "Syria"
+}, {
+    code: "TL",
+    name: "Timor"
+}, {
+    code: "VA",
+    name: "Vatican"
+}])
 
 function rgbToHex(r, g, b) {
     const rgb = (r << 16) | (g << 8) | (b << 0);
@@ -10,6 +94,7 @@ function rgbToHex(r, g, b) {
 
 export default function GeographicCoverage(props) {
     const [usaData, setUsaData] = useState(null);
+    const [worldData, setWorldData] = useState(null);
     const [maxCase, setMaxCase] = useState(null);
     const [minCase, setMinCase] = useState(null);
 
@@ -23,44 +108,74 @@ export default function GeographicCoverage(props) {
             .then(data => {
                 setUsaData(data);
                 
-                // set max usa case number
+                // set max and min case number
                 let total_usa_cases = data.map(datum => datum.total_cases);
                 setMaxCase(Math.max.apply(null, total_usa_cases));
                 setMinCase(Math.min.apply(null, total_usa_cases));
+            })
+            .catch(error => console.log(error));
+
+        // Request world data
+        const requestWorld = {
+            method: 'GET',
+        };
+        fetch(`${props.backendUrl}/covid-world-snap`, requestWorld)
+            .then(response => response.json())
+            .then(data => {
+                setWorldData(data);
             })
             .catch(error => console.log(error));
     }, []);
 
     function _getColorByCaseNum(caseNum) {
         const maxProportion = 1;
-        const minProportion = 0.2;
+        const minProportion = 0;
         const proportion = (maxProportion - minProportion) * (caseNum - minCase) / (maxCase - minCase) + minProportion;
-        return rgbToHex(255 * proportion, 0, 0);
+        return rgbToHex(255, 192 * (1 - proportion), 203 * (1 - proportion));
     }
 
     function mapHandler(event) {
         const stateData = usaData.filter(datum => datum.state === event.target.dataset.name)[0];
         const display = document.getElementById("geographic-coverage");
-        display.textContent = `${stateData.total_cases} total COVID cases in ${stateData.state}`;
+        display.textContent = `${stateData.state} ${stateData.total_cases}`;
     };
 
-    function statesCustomConfig() {
-        var config = {}
+    function getStatesCustomConfig() {
+        var config = {};
 
         if (usaData) {
             for (var i = 0; i < usaData.length; i++) {
                 config[usaData[i].state] = { fill: _getColorByCaseNum(usaData[i].total_cases) }
             }
         }
-
         return config;
+    };
+
+    function getWorldData() {
+        var data = [];
+        if (worldData) {
+            for (var i = 0; i < worldData.length; i++) {
+                let countryCode = getCode(worldData[i].country_name);
+                if (countryCode) {
+                    data.push({country: countryCode, value: worldData[i].total_cases });
+                }
+            }
+        }
+
+        return data;
     };
 
     return (
         <div>
             <h1>Geographic Coverage</h1>
-            <USAMap customize={statesCustomConfig()} onClick={mapHandler} />
+            <USAMap customize={getStatesCustomConfig()} onClick={mapHandler} />
             <p id="geographic-coverage"></p>
+            <WorldMap
+                color="red"
+                value-suffix="total cases"
+                size="xxl"
+                data={getWorldData()}
+            />
         </div>
     );
 }
