801ae1039064c4fdab7414dc327a1bc5bb30ef91
Olivia <xchen@mettel.net>
2022-04-12 19:54:56 -0400

    add race coverage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import './styles/App.css';
 import GeographicCoverage from './components/GeographicCoverage.js';
 import HistoricCoverage from './components/HistoricCoverage.js';
+import RaceCoverage from './components/RaceCoverage.js';
 
 function App() {
-  const url = "http://127.0.0.1:8088";
+  // const url = "http://127.0.0.1:8088";
+  const url = "http://127.0.0.1:5000";
 
   return (
     <div className="App">
       <GeographicCoverage backendUrl={url} />
       <HistoricCoverage backendUrl={url} />
+      <RaceCoverage backendUrl={url} />
     </div>
   );
 }
